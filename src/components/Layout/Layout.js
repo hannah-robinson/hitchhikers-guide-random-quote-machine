9022be9a32578675e77438c493289d3e4a13e5d0
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-02-13 12:45:12 +1300

    Add quote card styling

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,10 @@ import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
 
 const layout = () => (
-  <Aux>
+  <main className={classes.Card} >
       <div>Quote</div>
       <div>Twitter, Copy, New Quote buttons</div>
-  </Aux>
+  </main>
 )
 
 export default layout;
