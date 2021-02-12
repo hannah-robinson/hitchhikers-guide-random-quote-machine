427c7946b231fcae060eeb6772a3c693e87b3c56
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-02-13 11:31:35 +1300

    Add fonts and background styling, add placeholders to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
+
+const layout = () => (
+  <Aux>
+      <div>Quote</div>
+      <div>Twitter, Copy, New Quote buttons</div>
+  </Aux>
+)
+
+export default layout;
