2e2b1b02ab24fd6d51a41b48654c74f7629839f9
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-02-13 17:11:41 +1300

    Improve card background color, add font styling to Quote

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -5,8 +5,8 @@ import QuoteGenerator from '../../containers/QuoteGenerator';
 const quote = (props) => {
   return (
     <div>
-      <div id="text"><i class="fas fa-quote-left"></i> {props.quote} <i class="fas fa-quote-right"></i></div>
-      <div id="author"><em>{props.author}</em></div>
+      <div id="text" className={classes.Text}><i class="fas fa-quote-left"></i> {props.quote} <i class="fas fa-quote-right"></i></div>
+      <div id="author" className={classes.Author}>{props.author}</div>
     </div>
   )
 }
