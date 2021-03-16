2e7bde29f675b1a449d37fb4888cebd54eaf97ca
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-03-16 15:17:07 +1300

    Add Aux.js and add back rest of quotes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './App.module.css'
 import QuoteGenerator from './containers/QuoteGenerator';
+import Aux from './hoc/Aux';
 
 function App() {
   return (
-  <div>
+  <Aux>
   <QuoteGenerator />
   <div className={classes.sig}><a href="https://twitter.com/HannahRobinson"><i class="fab fa-twitter"></i>  @HannahRobinson</a></div>
-  </div>
+  </Aux>
   );
 }
 
