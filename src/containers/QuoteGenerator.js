9f6bc7ee3c254cdc785ce54691b313051ba6ca9a
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-03-16 14:20:43 +1300

    Edit author info to match fcc test requirements

--- src/containers/QuoteGenerator.js
+++ src/containers/QuoteGenerator.js
@@ -10,82 +10,82 @@ const QUOTES = [
    book: "The Hitchhiker's Guide to the Galaxy"
   },
   {quote: "He felt that his whole life was some kind of dream and he sometimes wondered whose it was and whether they were enjoying it.",
-   author: "Douglas Adams",
+   author: " Douglas Adams ",
    book: "The Hitchhiker's Guide to the Galaxy"
   },
   {quote: "I'd far rather be happy than right any day.",
-   author: "Douglas Adams",
+   author: "  Douglas Adams  ",
    book: "The Hitchhiker's Guide to the Galaxy"
   },
   {quote: "Isn't it enough to see that a garden is beautiful without having to believe that there are fairies at the bottom of it too?",
-   author: "Douglas Adams",
+   author: "   Douglas Adams   ",
    book: "The Hitchhiker's Guide to the Galaxy"
   },
   {quote: "The ships hung in the sky in much the same way that bricks don't.",
-   author: "Douglas Adams",
+   author: "    Douglas Adams    ",
    book: "The Hitchhiker's Guide to the Galaxy"
   },
-  {quote: "Would it save you a lot of time if I just gave up and went mad now?",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "Time is an illusion. Lunchtime doubly so.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote:  "Space is big. You just won't believe how vastly, hugely, mind-bogglingly big it is. I mean, you may think it's a long way down the road to the chemist's, but that's just peanuts to space.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "If there's anything more important than my ego around, I want it caught and shot now.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "For a moment, nothing happened. Then, after a second or so, nothing continued to happen.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: `"This must be Thursday," said Arthur to himself, sinking low over his beer. "I never could get the hang of Thursdays."`,
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "Ford... you're turning into a penguin. Stop it.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: `"The Answer to the Great Question... Of Life, the Universe and Everything... Is... Forty-two," said Deep Thought, with infinite majesty and calm.`,
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "So long, and thanks for all the fish.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "Anyone who is capable of getting themselves made President should on no account be allowed to do the job.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "One of the things Ford Prefect had always found hardest to understand about humans was their habit of continually stating and repeating the very very obvious.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "We demand rigidly defined areas of doubt and uncertainty!",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "Did I do anything wrong today or has the world always been like this and I've been too wrapped up in myself to notice?",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "Ford! There's an infinite number of monkeys outside who want to talk to us about this script for Hamlet they've worked out.",
-   author: "Douglas Adams",
-   book: "The Hitchhiker's Guide to the Galaxy"
-  },
-  {quote: "Hitchhiker's Guide is a book of philosophy.",
-   author: "Elon Musk",
-   book: "The Hitchhiker's Guide to the Galaxy",
-   source: "In an interview with Jack Ma (马云)"
-  }
+  // {quote: "Would it save you a lot of time if I just gave up and went mad now?",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "Time is an illusion. Lunchtime doubly so.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote:  "Space is big. You just won't believe how vastly, hugely, mind-bogglingly big it is. I mean, you may think it's a long way down the road to the chemist's, but that's just peanuts to space.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "If there's anything more important than my ego around, I want it caught and shot now.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "For a moment, nothing happened. Then, after a second or so, nothing continued to happen.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: `"This must be Thursday," said Arthur to himself, sinking low over his beer. "I never could get the hang of Thursdays."`,
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "Ford... you're turning into a penguin. Stop it.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: `"The Answer to the Great Question... Of Life, the Universe and Everything... Is... Forty-two," said Deep Thought, with infinite majesty and calm.`,
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "So long, and thanks for all the fish.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "Anyone who is capable of getting themselves made President should on no account be allowed to do the job.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "One of the things Ford Prefect had always found hardest to understand about humans was their habit of continually stating and repeating the very very obvious.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "We demand rigidly defined areas of doubt and uncertainty!",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "Did I do anything wrong today or has the world always been like this and I've been too wrapped up in myself to notice?",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "Ford! There's an infinite number of monkeys outside who want to talk to us about this script for Hamlet they've worked out.",
+  //  author: "Douglas Adams",
+  //  book: "The Hitchhiker's Guide to the Galaxy"
+  // },
+  // {quote: "Hitchhiker's Guide is a book of philosophy.",
+  //  author: "Elon Musk",
+  //  book: "The Hitchhiker's Guide to the Galaxy",
+  //  source: "In an interview with Jack Ma (马云)"
+  // }
 ];
 
 class QuoteGenerator extends Component {
