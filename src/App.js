5127f7804318c330304aa5a533449d5ed28db209
Hannah Robinson <hannah.robinson.git@gmail.com>
2021-03-16 14:18:20 +1300

    Add link to my Twitter and freeCodeCamp's testing bundle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import classes from './App.module.css'
 import QuoteGenerator from './containers/QuoteGenerator';
 
 function App() {
   return (
+  <div>
   <QuoteGenerator />
+  <div className={classes.sig}><a href="https://twitter.com/HannahRobinson"><i class="fab fa-twitter"></i>  @HannahRobinson</a></div>
+  </div>
   );
 }
 
